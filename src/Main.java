9b51019e021d988f05b55d0525766f5b8744359b
Emerson Carlos <emcchost@gmail.com>
2020-05-15 19:28:41 -0300

    Cabecalho

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main{
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("SISTEMA DE PERFIL DE CLIENTE ");
+		System.out.println("---------------------------------- ");
+		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO  ");
+		sc.close();
+	}
+	
+}
